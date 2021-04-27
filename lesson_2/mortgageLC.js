edffb3c710e09045af5149319b692d7d540fc73a
Miles Abbason <miles.abbason@gmail.com>
2021-04-27 15:40:33 -0400

    Refactor of mortgageLS.js

--- lesson_2/mortgageLC.js
+++ lesson_2/mortgageLC.js
@@ -16,54 +16,64 @@ Output monthly payment, extra stuff might be fun, total interest, total cost
 
 let readline = require('readline-sync');
 
+const MONTHS_IN_YEAR = 12;
+const LOAN_MAX = 5000000;
+
 function invalidEntry(numString) {
   let num = parseFloat(numString);
   return (Number.isNaN(num) || num <= 0);
 }
 
 
-//User input loan amount, test for valid user entry
-let loanAmount = Math.round(parseFloat(readline.question('Please enter desired loan amount to the nearest dollar: $')));
+function getLoanAmount() {
+  let loanAmount = Math.round(parseFloat(readline.question('Please enter desired loan amount to the nearest dollar: $')));
 
-let loanMax = 5000000;
-while (loanAmount > loanMax) {
-  loanAmount = Math.round(parseFloat(readline.question(`You're barkin' up the wrong bank, try a lower amount: $`)));
+  while (loanAmount > LOAN_MAX) {
+    loanAmount = Math.round(parseFloat(readline.question(`You're barkin' up the wrong bank, try a lower amount: $`)));
+  }
+  while (invalidEntry(loanAmount) || loanAmount > LOAN_MAX) {
+    loanAmount = Math.round(parseFloat(readline.question(`Please enter a valid loan amount: $`)));
+  }
+
+  return loanAmount;
 }
-while (invalidEntry(loanAmount) || loanAmount > loanMax) {
-  loanAmount = Math.round(parseFloat(readline.question(`Please enter a valid loan amount: $`)));
+
+function getAPR() {
+  let APR = parseFloat(readline.question('Please enter the Annual Percentage Rate (APR) as a percent eg. 3.75%: '));
+
+  while (invalidEntry(APR)) {
+    APR = parseFloat(readline.question(`Please enter a valid interest rate: `));
+  }
+  APR /= 100;
+
+  return APR;
 }
 
+function getLoanLengthYears() {
+  let loanLength = parseFloat(readline.question('Please enter loan length in years: '));
 
-//User input APR, test for valid entry, set monthly rate
-let annualRate = parseFloat(readline.question('Please enter the Annual Percentage Rate (APR) as a percent eg. 3.75%: '));
-
-while (invalidEntry(annualRate)) {
-  annualRate = parseFloat(readline.question(`Please enter a valid interest rate: `));
+  while (invalidEntry(loanLength) || !Number.isInteger(loanLength)) {
+    loanLength = parseFloat(readline.question('Please enter valid whole number for the length of loan in years: '));
+  }
+  return loanLength;
 }
-annualRate /= 100;
-let monthlyRate = annualRate / 12;
 
+console.log(`\nWelcome to Mortgage Loan Calculator!\n`);
 
-//User input loan length, test for valid user entry
-let loanLength = parseFloat(readline.question('Please enter loan length in years: '));
-
-while (invalidEntry(loanLength) || Number.isInteger(loanLength)) {
-  loanLength = readline.question('Please enter valid whole number for the length of loan in years: ');
-}
-loanLength *= 12;
-
+let loanAmount = getLoanAmount();
+let monthlyRate = getAPR() / MONTHS_IN_YEAR;
+let loanLengthMonths = getLoanLengthYears() * MONTHS_IN_YEAR;
 
 //Use all valid inputs for calculation
 let monthlyPayment = (loanAmount * (monthlyRate / (1 - Math.pow(
-  (1 + monthlyRate), (-loanLength))))).toFixed(2);
+  (1 + monthlyRate), (-loanLengthMonths))))).toFixed(2);
 
-let totalInterest = ((monthlyPayment * loanLength) - loanAmount).toFixed(2);
+let totalInterest = ((monthlyPayment * loanLengthMonths) -
+                    loanAmount).toFixed(2);
 
 
 //Output calculations
 console.log(`\nLoan Calculation Results:\n
   Payment Every Month: $${monthlyPayment}\n
-  Total of ${loanLength} Payments: $${(monthlyPayment * loanLength).toFixed(2)}\n
+  Total of ${loanLengthMonths} Payments: $${(monthlyPayment * loanLengthMonths).toFixed(2)}\n
   Total Interest: $${totalInterest}`);
-
-console.log(loanLength);
